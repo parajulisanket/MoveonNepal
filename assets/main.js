ca13bde8cd2e3b25565e83bd29c61f9d87c2907f
sanketparajuli <parajulisanket7@gmail.com>
2025-05-30 16:46:49 +0545

    changes on index page

--- assets/main.js
+++ assets/main.js
@@ -160,3 +160,17 @@ document.querySelectorAll(".video-wrapper").forEach((wrapper) => {
       : "fa-solid fa-volume-high volume-icon";
   });
 });
+
+// for index hero section images moving in 5 sec script
+const slides = document.querySelectorAll("#slideshow img");
+let current = 0;
+
+setInterval(() => {
+  slides[current].classList.remove("opacity-100");
+  slides[current].classList.add("opacity-0");
+
+  current = (current + 1) % slides.length;
+
+  slides[current].classList.remove("opacity-0");
+  slides[current].classList.add("opacity-100");
+}, 5000);
